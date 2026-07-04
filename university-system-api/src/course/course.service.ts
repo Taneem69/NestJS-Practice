c62ca3c5c51d823bd05294851af7db6df9ffb935
Taneem69 <tanvirtaneem32@gmail.com>
2026-07-04 13:45:34 +0600

    fixed the name of the method

--- university-system-api/src/course/course.service.ts
+++ university-system-api/src/course/course.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CourseService {
-    getAllCourse(){
+    getAllCourses(){
         return {message: 'All courses feteched', data: []};
     }
 
