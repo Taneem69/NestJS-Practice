c2cc13d2cc424944ae6573bfe79e18941d6c5e86
Taneem69 <tanvirtaneem32@gmail.com>
2026-07-04 15:01:37 +0600

    Updated enrollment service file

--- university-system-api/src/enrollment/enrollment.service.ts
+++ university-system-api/src/enrollment/enrollment.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject, forwardRef } from '@nestjs/common';
 import { CourseService } from 'src/course/course.service';
+import { NotificationService } from 'src/notification/notification.service';
 
 @Injectable()
 export class EnrollmentService {
-    constructor(private readonly courseService: CourseService){}
+    constructor(private readonly courseService: CourseService, @Inject(forwardRef(()=> NotificationService)) private readonly notificationService: NotificationService){}
 
     getEnrollments(){
         return {message: 'All enrollments fetched', data: []};
@@ -12,11 +13,14 @@ export class EnrollmentService {
     enrollStudent(studentName: string, courseId: string){
         const course=this.courseService.getCourseById(courseId);
 
+        const notification= this.notificationService.sendNotification(studentName, `enrolled in course ${courseId}`,);
+
         return {
             message: 'Student enrolled successfully',
 
             student: studentName,
             course,
+            notification,
         };
     }
 }
