1d283478c93920f4148c3660576509b0ed653b36
Taneem69 <tanvirtaneem32@gmail.com>
2026-07-04 14:07:33 +0600

    Part/B/InterModule(Imported course module in enrollment module)

--- university-system-api/src/enrollment/enrollment.module.ts
+++ university-system-api/src/enrollment/enrollment.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { CourseModule } from 'src/course/course.module';
 import { EnrollmentService } from './enrollment.service';
 import { EnrollmentController } from './enrollment.controller';
 
 @Module({
+  imports: [CourseModule],
   controllers: [EnrollmentController],
   providers: [EnrollmentService],
 })
