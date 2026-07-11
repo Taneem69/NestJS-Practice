6011979830bbee6cdf52b0024e15c5d43bb1afa2
Taneem69 <tanvirtaneem32@gmail.com>
2026-07-11 22:05:56 +0600

    ...

--- course-management-api/src/course/dto/create-course.dto.ts
+++ course-management-api/src/course/dto/create-course.dto.ts
@@ -1,37 +1,31 @@
 import { 
-    IsString,
-    IsNotEmpty,
-    IsNumber,
-    Min,
-    Max,
-    IsOptional
- } from "class-validator";
+   IsString,
+   IsNotEmpty,
+   IsNumber,
+   Min,
+   Max,
+   IsOptional
+} from "class-validator";
 
- import{Type} from 'class-transformer';
+import{Type} from 'class-transformer';
 
- export class CreateCourseDto{
+export class CreateCourseDto{
 
-    @IsString()
-    @IsNotEmpty({message: 'name should not be empty'})
-    name: string;
-
-    @IsString()
-    @IsNotEmpty({message: 'code should not be empty'})
-    code: string;
-
-    @IsString()
-    @IsNotEmpty({message: 'instructor should not be empty'})
-    instructor: string;
-
-
-    @IsNumber()
-    @Min(1, {message: 'Credits must be at least 1'})
-    @Max(6, {message: 'Credits must not be greater than 6'})
-    @Type(()=>Number)
-    credits: number;
-
-
-    @IsOptional()
-    @IsString()
-    description?: string;
- }
+   @IsString()
+   @IsNotEmpty({message: 'name should not beempty'})
+   name: string;
+   @IsString()
+   @IsNotEmpty({message: 'code should not beempty'})
+   code: string;
+   @IsString()
+   @IsNotEmpty({message: 'instructor should notbe empty'})
+   instructor: string;
+   @IsNumber()
+   @Min(1, {message: 'Credits must be at least1'})
+   @Max(6, {message: 'Credits must not be greaterthan 6'})
+   @Type(()=>Number)
+   credits: number;
+   @IsOptional()
+   @IsString()
+   description?: string;
+}
