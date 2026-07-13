b7d160634ec7a8d68e06a9f98df0f0279aabd322
Taneem69 <tanvirtaneem32@gmail.com>
2026-07-13 13:07:08 +0600

    setted up the connection with postgres

--- product-inventory-api/src/app.module.ts
+++ product-inventory-api/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '370422',
+      database: 'product_inventory_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ProductsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
